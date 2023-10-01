ef8af0738e185b09d6b5f0c4b7da4abf980c5410
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-02 06:56:26 +0900

    remove unused var

--- buffer.go
+++ buffer.go
@@ -9,13 +9,13 @@
 package mysql
 
 import (
-	"io"
 	"net"
 	"time"
 )
 
-const defaultBufSize = 4096
-const maxCachedBufSize = 256 * 1024
+const defaultBufSize = 4 * 1024
+
+// const maxCachedBufSize = 256 * 1024
 
 // A buffer which is used for both reading and writing.
 // This is possible since communication on each connection is synchronous.
@@ -26,7 +26,6 @@ const maxCachedBufSize = 256 * 1024
 type buffer struct {
 	buf     []byte // buf is a byte buffer who's length and capacity are equal.
 	nc      net.Conn
-	idx     int
 	length  int
 	timeout time.Duration
 	dbuf    [2][]byte // dbuf is an array with the two byte slices that back this buffer
@@ -48,83 +47,6 @@ func newBuffer(nc net.Conn) buffer {
 // the actual flip will be performed the next time we call `buffer.fill`
 func (b *buffer) flip() {
 	b.flipcnt += 1
-}
-
-// fill reads into the buffer until at least _need_ bytes are in it
-func (b *buffer) fill(need int) error {
-	n := b.length
-	// fill data into its double-buffering target: if we've called
-	// flip on this buffer, we'll be copying to the background buffer,
-	// and then filling it with network data; otherwise we'll just move
-	// the contents of the current buffer to the front before filling it
-	dest := b.dbuf[b.flipcnt&1]
-
-	// grow buffer if necessary to fit the whole packet.
-	if need > len(dest) {
-		// Round up to the next multiple of the default size
-		dest = make([]byte, ((need/defaultBufSize)+1)*defaultBufSize)
-
-		// if the allocated buffer is not too large, move it to backing storage
-		// to prevent extra allocations on applications that perform large reads
-		if len(dest) <= maxCachedBufSize {
-			b.dbuf[b.flipcnt&1] = dest
-		}
-	}
-
-	// if we're filling the fg buffer, move the existing data to the start of it.
-	// if we're filling the bg buffer, copy over the data
-	if n > 0 {
-		copy(dest[:n], b.buf[b.idx:])
-	}
-
-	b.buf = dest
-	b.idx = 0
-
-	for {
-		if b.timeout > 0 {
-			if err := b.nc.SetReadDeadline(time.Now().Add(b.timeout)); err != nil {
-				return err
-			}
-		}
-
-		nn, err := b.nc.Read(b.buf[n:])
-		n += nn
-
-		switch err {
-		case nil:
-			if n < need {
-				continue
-			}
-			b.length = n
-			return nil
-
-		case io.EOF:
-			if n >= need {
-				b.length = n
-				return nil
-			}
-			return io.ErrUnexpectedEOF
-
-		default:
-			return err
-		}
-	}
-}
-
-// returns next N bytes from buffer.
-// The returned slice is only guaranteed to be valid until the next read
-func (b *buffer) readNext(need int) ([]byte, error) {
-	if b.length < need {
-		// refill
-		if err := b.fill(need); err != nil {
-			return nil, err
-		}
-	}
-
-	offset := b.idx
-	b.idx += need
-	b.length -= need
-	return b.buf[offset:b.idx], nil
 }
 
 // takeBuffer returns a buffer with the requested size.
